cfe33efe50a72f48ff3973d2ee0719f2cf1935e7
BestKora <developer@bestkora.com>
2014-07-12 19:08:10 +0400

    Initial Commit

---
+++ Assignment-2-iOS7/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Assignment-2-iOS7
+//
+//  Created by Tatiana Kornilova on 7/12/14.
+//  Copyright (c) 2014 Tatiana Kornilova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Assignment-2-iOS7/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Assignment-2-iOS7
+//
+//  Created by Tatiana Kornilova on 7/12/14.
+//  Copyright (c) 2014 Tatiana Kornilova. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
